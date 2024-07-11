57dec3da4b6c69ed184c196376db6ae6cb654e2f
gusdn0413 <gusdn0413@naver.com>
2024-07-11 11:04:09 +0900

    데이터 접근 기술 - 스프링 데이터 JPA

--- src/main/java/hello/itemservice/service/ItemServiceV1.java
+++ src/main/java/hello/itemservice/service/ItemServiceV1.java
@@ -4,6 +4,7 @@ import hello.itemservice.domain.Item;
 import hello.itemservice.repository.ItemRepository;
 import hello.itemservice.repository.ItemSearchCond;
 import hello.itemservice.repository.ItemUpdateDto;
+import hello.itemservice.repository.jpa.SpringDataJpaItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -18,6 +19,7 @@ public class ItemServiceV1 implements ItemService {
 
     @Override
     public Item save(Item item) {
+//        return itemRepository.save(item);
         return itemRepository.save(item);
     }
 
